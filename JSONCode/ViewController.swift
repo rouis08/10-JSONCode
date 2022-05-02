9afaf96aeaee4479881f286113aa05f2aed00f8a
LouisLee <rouis08@yahoo.com.tw>
2022-05-03 01:00:48 +0800

    I have tried one whole day playing around the Json format because another webEnglish app has a serious incorrect Json format problem. I still can't fix it. Anyway I would like to store all what I have tried today about Json format and URLRequest.

--- JSONCode/ViewController.swift
+++ JSONCode/ViewController.swift
@@ -25,12 +25,7 @@ struct AQI: Codable {
 
 #else
 struct purchase: Codable {
-//    enum CodingKeys : String, CodingKey {
-//        case county = "County"
-//        case sitename = "SiteName"
-//        case aqi = "AQI"
-//    }
-    
+   
     var id: String
     var item: String
     var price: String
@@ -41,8 +36,6 @@ struct purchase: Codable {
     var location: String
     var payment: String
     var note: String
-    
-
 }
 #endif
 
@@ -52,20 +45,60 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        var i:Int32 = 0
+        i = i+1
+        print("program starts \(i)")
         #if false
-        let url = URL(string: "https://opendata.epa.gov.tw/ws/Data/AQI/?$format=json")
+            let url = URL(string: "https://opendata.epa.gov.tw/ws/Data/AQI/?$format=json")
+            let data = try! Data(contentsOf: url!)
+            let aqi = try! JSONDecoder().decode([AQI].self, from: data)
         #else
-        // This is to receive my created page.
-        let url = URL(string: "http://localhost/read_purchase_json2.php")
-        #endif
-        let data = try! Data(contentsOf: url!)
 
-        let aqi = try! JSONDecoder().decode([purchase].self, from: data)
+            let url = URL(string: "http://localhost/read_purchase_json2.php")
+            //llee20220421 I will get an error if using let url = URL(string: "http://localhost/read_purchase_json2.php")
+            
+            let data = try! Data(contentsOf: url!)
+        
+            print("---0--\n")
+            print(data)
+        
+            let html = String(data: data, encoding: .utf8)
+            print("---1--\n")
+            print(html)
+            print("---1.1--\n")
+            print(html!)
+            
+            print("---1.2--\n")
+            print(data as NSData)
+        
+            //let aqi = try! JSONDecoder().decode([purchase].self, from: data)
+        do {
+                 let aqi = try JSONDecoder().decode([purchase].self, from: data)
+                 print("---2--\n")
+                 print(aqi)
 
-        aqi.forEach { (p) in
-            print(p)
+                 print("---3--\n")
+                 aqi.forEach { (p) in
+                    print(p)
+                 }
+
+           } catch  {
+                 print(" enter error routine \n")
+                 print(error)
+                 print(data)
         }
+        
+        
+         //   print("---2--\n")
+         //   print(aqi)
+                
+         //   print("---3--\n")
+         //   aqi.forEach { (p) in
+         //       print(p)
+         //   }
+        #endif
+
     }
 }
 
